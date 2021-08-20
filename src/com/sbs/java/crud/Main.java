20f50a321509fe21ae9a9bb9760a1e0e34968cf4
karis1234 <qkzkf3487@naver.com>
2021-08-20 18:50:02 +0900

    게시물 작성 기능 구현중

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -1,31 +1,39 @@
 package com.sbs.java.crud;
 
-import java.util.Scanner; // ctrl + shift + 0
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("명령어 : ");
-		// 스캐너
-		// Scanner의 객체를 만들고 'sc'라는 리모컨에 연결한다.
-		// 이런 'sc'를 통해서 사용자가 커맨드를 입력하기를 기다린다.
-		String command = sc.nextLine().trim();
-		// 한줄을 받는다. 즉 입력을 기다리고 있는 중이다.
-		// nextLine -> 한줄을 받는다!
-		// 만약 nextLine에서 Line을 빼면 첫번째 명령어(띄어쓰기 전까지의)를 받는다.
-		// trim() -> 문자 양쪽의 쓸때없는 공백을 지운다.(문자 중간의 공백은 지우지 않는다.)
-		System.out.printf("입력된 명령어 : %s\n", command);
-		// 입력한 명령어를 낚아채서 출력한다.
-		// 입력한 명령어가 2개 이상있으면 마지막 명령어를 출력한다.
+		int lastArticleId = 0;
 
-//		System.out.printf("명령어 : ");
+		while (true) {
+			System.out.printf("명령어 : ");
+			String command = sc.nextLine().trim();
+			if (command.length() == 0) {
+				continue;
+			}
+			if (command.equals("system.exit")) {
+				break;
+			}
+			if (command.equals("article.write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
 
-//		int number = sc.nextInt();
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 
-//		System.out.printf("입력된 숫자 : %d\n", number);
-		
+			}else if(command.equals("article.list")) {
+				System.out.printf("게시물이 없습니다.\n");
+			}else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n",command);
+			}
+
+		}
 
 		sc.close();
 
